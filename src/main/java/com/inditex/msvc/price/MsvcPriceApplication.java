d1da1ca6e2d1767f524749ef3e6d20246a67abf1
unknown <Edwin Capillo>
2025-07-09 01:01:48 -0500

    TK-V1: Starting the project

---
+++ src/main/java/com/inditex/msvc/price/MsvcPriceApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.msvc.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcPriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/msvc/price/MsvcPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.msvc.price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
